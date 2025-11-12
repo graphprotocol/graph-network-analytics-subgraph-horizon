ee4e5b2a8b3c1f7d33db9f2d444c4da9c8f2a7b4
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2025-11-11 21:04:27 -0300

    fix: scripts and delegatedThawingTokens

--- src/mappings/horizonStaking.ts
+++ src/mappings/horizonStaking.ts
@@ -549,6 +549,7 @@ export function handleTokensUndelegated(event: TokensUndelegated): void {
     if (provision.delegatorShares != BigInt.fromI32(0)) {
         provision = updateDelegationExchangeRateForProvision(provision as Provision)
     }
+    provision.delegatedThawingTokens = provision.delegatedThawingTokens.plus(event.params.tokens)
     provision = updateAdvancedProvisionMetrics(provision as Provision)
     provision.save()
 
@@ -559,6 +560,7 @@ export function handleTokensUndelegated(event: TokensUndelegated): void {
     if (indexer.delegatorShares != BigInt.fromI32(0)) {
         indexer = updateDelegationExchangeRate(indexer as Indexer)
     }
+    indexer.delegatedThawingTokens = indexer.delegatedThawingTokens.plus(event.params.tokens)
     indexer = updateAdvancedIndexerMetrics(indexer as Indexer)
     indexer = calculateCapacities(indexer as Indexer)
     indexer.save()
@@ -620,12 +622,14 @@ export function handleDelegatedTokensWithdrawn(event: DelegatedTokensWithdrawn):
     let provision = createOrLoadProvision(event.params.serviceProvider, event.params.verifier, event.block.timestamp)
     // might want to track locked/thawing tokens in provision too
     provision.delegatedTokens = provision.delegatedTokens.minus(event.params.tokens)
+    provision.delegatedThawingTokens = provision.delegatedThawingTokens.minus(event.params.tokens)
     provision.save()
 
     let indexerID = event.params.serviceProvider.toHexString()
     let indexer = Indexer.load(indexerID)!
 
     indexer.delegatedTokens = indexer.delegatedTokens.minus(event.params.tokens)
+    indexer.delegatedThawingTokens = indexer.delegatedThawingTokens.minus(event.params.tokens)
     indexer = calculateCapacities(indexer as Indexer)
     indexer.save()
 
